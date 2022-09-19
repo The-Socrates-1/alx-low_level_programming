26a616f4556d6c6212c862eeef17424c9fd36dec
The-Socrates-1 <olusola92@yahoo.com>
2022-09-19 16:00:01 -0700

    corrections to 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,23 +5,22 @@
  * @s: the string to be changed
  *
  * Return: the converted int
- * */
+ */
 
 int _atoi(char *s)
 {
 int sign = 1;
 unsigned int num = 0;
-do
-{
-if
-(*s == '-')
+
+do {
+if (*s == '-')
 sign *= -1;
-else if 
-(*s >= '0' && *s <= '9')
+else if (*s >= '0' && *s <= '9')
 num = num * 10 + (*s - '0');
 else if (num > 0)
 break;
-}
-while (*s++);
+} while (*s++);
+
 return (num * sign);
+
 }
